8ac13c6f0432e014947de6c416e122f13a0e3077
Ethan Schutzenhofer <ews624@mail.missouri.edu>
2020-02-05 10:21:11 -0600

    Finished Module2

--- Module2/objectposncalc.py
+++ Module2/objectposncalc.py
@@ -1,8 +1,55 @@
 print('Ethan\'s Object Position Calculator')
-pos = float(input('Enter Initial Position in m: '))
-velo = float(input('Enter Initial Velocity in m/s: '))
-accel = float(input('Enter acceleration in m/s^2: '))
-time = float(input('Enter the amount of time in s: '))
 
-final_pos = pos + (velo * time) + (.5 * accel * time * time)
-print('Final Position is : ', final_pos) 
+program_run = True
+
+while(program_run):
+    while(True):
+        try:
+            pos = float(input('Enter Initial Position in m: '))
+            if(pos < 0):
+                print('Negative values are not allowed')
+                continue
+        except ValueError:
+            print('The value you entered is invalid. Only numerical values are valid')
+        else:
+            break
+
+    while(True):
+        try:
+            velo = float(input('Enter Initial Velocity in m/s: '))
+            if(velo < 0):
+                print('Negative values are not allowed')
+                continue
+        except ValueError:
+            print('The value you entered is invalid. Only numerical values are valid')
+        else:
+            break
+        
+    while(True):
+        try:
+            accel = float(input('Enter acceleration in m/s^2: '))
+            if(accel < 0):
+                print('Negative values are not allowed')
+                continue
+        except ValueError:
+            print('The value you entered is invalid. Only numerical values are valid')
+        else:
+            break
+        
+    while(True):
+        try:
+            time = float(input('Enter the amount of time in s: '))
+            if(time < 0):
+                print('Negative values are not allowed')
+                continue
+        except ValueError:
+            print('The value you entered is invalid. Only numerical values are valid')
+        else:
+            break
+    
+    final_pos = pos + (velo * time) + (.5 * accel * time * time)
+    print('Final Position is : ', final_pos) 
+
+    another_one= input('Do you want to do another calculation? (y/n): ')
+    if(another_one != 'y'):
+            program_run = False
