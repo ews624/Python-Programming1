1acf155df6a1dbc96591dca3bf6da4f96ca15e67
Ethan Schutzenhofer <ews624@mail.missouri.edu>
2020-04-29 22:25:11 -0500

    Finished

--- Module13/rps.py
+++ Module13/rps.py
@@ -1,20 +1,8 @@
 import random
 
+gameDictionary = dict({1: "Rock", 2:"Paper", 3:"Scissors"})
+resultDictionary = dict({1: "win", -1: "lose", 0: "tie"})
 
-class Record:
-    def __init__(self, name,wins,losses):
-        self.__name = name
-        self.__wins = wins
-        self.__losses = losses
-
-    def get_name(self):
-        return self.__name
-
-    def get_wins(self):
-        return self.__wins
-
-    def get_losses(self):
-        return self.__losses
 
 def print_Start_Menu():
     print("Welcome to Rock, Paper, Scissors!")
@@ -28,11 +16,15 @@ def print_RPS():
     print("2.Paper")
     print("3.Scissors")
 
+def print_Menu2():
+    print("\n1. Play again")
+    print("2. View Statistics")
+    print("3. Quit")
 
 def get_RPS_choice():
     while True:
         try:
-            choice = int(input("\nWhat would you like to do?"))
+            choice = int(input("\nEnter choice: "))
             if choice < 1 or choice > 3:
                 print(choice," is not a valid choice.")
                 continue
@@ -43,68 +35,101 @@ def get_RPS_choice():
         
     return choice
 
-def check_for_RPS_file():
-    fileName = "statistics.rps"
+def check_for_RPS_file(name):
+    fileName = name + ".rps"
     try:
         f = open(fileName, "r+")
-
     except:
         f = open(fileName, "x+")
+        f.close()
         return
-    for record in f:
-        token = record.split('+')
-        stats = Record(token[0], int(token[1]), int(token[2]))
-        timeline.append(stats)
     f.close()
 
 
-def play_RPS():
-
-    name = str(input("What is your name?"))
-    wins = 0
-    losses = 0
-    for records in timeline:
-        if name in records.get_name():
-            wins = records.get_wins()
-            losses = recoords.get_losses()
-            
-def create_RPS_Game():
-
+def load_RPS():
     name = str(input("\nWhat is your name?"))
-    newRecords = Record(name, 0,0)
-    timeline.append(newRecords)
-    RPS_Game_Play(name)
+    fileName = name+".rps"
+    try:
+        with open(fileName) as f:
+            stats = f.readlines()
+        ##finish later
 
-def New_RPS():
-    check_for_RPS_file()
-    create_RPS_Game()
-    play_RPS()
+        print("Welcome back ", name,". Let's Play!")
+    except:
+        print(name, ", your game could not be found")
+    
+    
+def simulate_Game(choice,cpu_choice):
 
-def simulate_Game(choice):
+    if (choice == 1) & (cpu_choice == 1):
+        return 0
+    elif (choice == 1) & (cpu_choice == 2):
+        return -1
+    elif (choice == 1) & (cpu_choice == 3):
+        return 1
+    elif (choice == 2) & (cpu_choice == 1):
+        return 1
+    elif (choice == 2) & (cpu_choice == 2):
+        return 0
+    elif (choice == 2) & (cpu_choice == 3):
+        return -1
+    elif (choice == 3) & (cpu_choice == 1):
+        return -1
+    elif (choice == 3) & (cpu_choice == 2):
+        return 1
+    elif (choice == 3) & (cpu_choice == 3):
+        return 0
+    
+def RPS_Game_Play():
+
+    print("Round ", stats[4])
+    print_RPS()
+    choice = int(input("\nWhat will it be? "))
+    while choice < 1 or choice > 3:
+        print_RPS()
+        choice = int(input("\nWhat will it be? "))
 
     cpu_choice = random.randint(1, 3)
+    
+    RPS_Result = simulate_Game(choice, cpu_choice)
 
-    if choice == cpu_choice:
-        print("You and the computer both choose " choice " Tie")
+    print("You chose " + gameDictionary[choice] + ". The computer chose " +
+          gameDictionary[cpu_choice] + ". You " + resultDictionary[RPS_Result])       
 
-    elif choice == 1 && cpu_choice == 2:
-        print("You chose " choice ". The computer chose
+    return RPS_Result
 
-   
-        
+def save_file():
+    try:
+        file = stats[0] + ".rps"
+        save_file = open(file, "w")
+        save_file.write(stats[0])
+        save_file.write("\n"+stats[1])
+        save_file.write("\n",stats[2])
+        print(stats[0] + ", your game has been saved.")
+    except:
+        print("Save Failed")
 
-def RPS_Game_Play(name):
-
-    for record in timeline:
-        if name in record.get_name():
-           roundNumber = record.get_wins() + record.get_losses() + 1 
-
-     
-    print("Round ", roundNumber)
-    print_RPS()
-    choice = str(input("\nWhat will it be? ")
+def New_RPS():
     
     
+    name = str(input("\nWhat is your name?"))
+    check_for_RPS_file(name)
+    print("Hello", name,". Let's Play!")
+    stats.append(name)
+    stats.append(0)
+    stats.append(0)
+    stats.append(0) 
+    stats.append(1)
+
+    result = RPS_Game_Play()
+    
+    if (result == -1):
+        stats[2] = stats[2] + 1
+    elif (result == 0):
+        stats[3] = stats[3] + 1
+    elif (result == 1):
+        stats[1] = stats[1] + 1
+    stats[4] = stats[4] + 1
     
     
 def main():
@@ -112,15 +137,46 @@ def main():
 
     choice = get_RPS_choice()
 
-
     if choice == 1:
         New_RPS()
 
     if choice == 2:
-        print()
+        load_RPS()
 
     if choice == 3:
-        print()
+        return
 
-timeline = []
+    while 1:
+        
+        print("\nWhat would you like to do? ")
+        print_Menu2()
+
+        choice2 = get_RPS_choice()
+        
+        if choice2 == 1:
+
+            result = RPS_Game_Play()
+            if (result == -1):
+                stats[2] = stats[2] + 1
+            elif (result == 0):
+                stats[3] = stats[3] + 1
+            elif (result == 1):
+                stats[1] = stats[1] + 1
+            stats[4] = stats[4] + 1
+
+        elif choice2 == 2:
+            print(stats[0] + ", here are your gameplay statistics...")
+            print("Wins:",stats[1])
+            print("Losses:",stats[2])
+            print("Ties:",stats[3])
+            try:
+                print("Win/Loss Ratio:",(float(stat[1])/float(stats[2])))
+            except:
+                print("Win/Loss Ratio:",0.0)
+
+        elif choice2 == 3:
+            save_file()
+            break
+
+stats = []
 main()
